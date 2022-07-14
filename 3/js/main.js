275aead868cc7b03053cb8435b46ce5af5e0eab0
Каруна Сергей <55633853+KarunaGray@users.noreply.github.com>
2022-07-14 20:35:46 +0300

    Update main.js

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,18 +1,40 @@
 
 const list = [ ];
 
+const $form = document.querySelector('form#user');
+const $input = $form.querySelector('input[name="user_name"]');
+const $button = $form.querySelector('button[name="submit_button"]');
+
 const $ul = document.querySelector('ul#list');
 //Удаление элемента
 const removeItem = ($element) => {
     $ul.removeChild($element);
 };
 
+//Разблокировка кнопки
+const UnlockSubmit = () => {
+    $button.disabled = false;
+    $button.classList.remove("disable");
+}
+
+//Проверка на количество ошибок и элементов
+isUnableToEnter = (key) => {
+    const $listOfItems = document.querySelectorAll('li');
+    const $listOfError = document.querySelectorAll('small');
+    if(key){
+        return ($listOfItems.length < 9) ? true : false;
+    }
+    else{
+        return (($listOfItems.length > 9)||($listOfError.length > 3)) ? true : false;
+    }
+}
+
 //Добавление элемента
 const addItem = (item) => {
     const $li = document.createElement('li');
     $li.innerText = item;
     $li.addEventListener('click', (event) => {
-        removeItem(event.target);
+        removeItem(event.target);UnlockSubmit();
     });
     $ul.appendChild($li);
 };
@@ -25,6 +47,8 @@ const addErrorItem = (item) => {
     $small.innerHTML += "<br>";
     setTimeout(() => {
         $ParentDiv.removeChild($small);
+        if(isUnableToEnter(true)){
+        UnlockSubmit();}
     }, 5000);
     $ParentDiv.insertBefore($small,$ul);
 };
@@ -33,17 +57,6 @@ const addErrorItem = (item) => {
 list.forEach((el, index) => {
     addItem(el);
 });
-
-const $form = document.querySelector('form#user');
-const $input = $form.querySelector('input[name="user_name"]');
-const $button = $form.querySelector('button[name="submit_button"]');
-
-//Проверка на количество ошибок и элементов
-isUnableToEnter = () => {
-    const $listOfItems = document.querySelectorAll('li');
-    const $listOfError = document.querySelectorAll('small');
-    return (($listOfItems.length > 9)||($listOfError.length > 3)) ? true : false;
-}
 
 //Ивент на кнопку (Добавление элемента\блокировка кнопки)
 $form.addEventListener('submit', (event) => {
@@ -57,29 +70,9 @@ $form.addEventListener('submit', (event) => {
         addErrorItem("Введено недостаточное количество символов");
     }
     
-    if(isUnableToEnter())
+    if(isUnableToEnter(false))
     {
         $button.disabled = true;
         $button.classList.add("disable");
     }
 });
-
-//Ивент на поле ввода(Разблокировка кнопки)
-$input.addEventListener('input', () => {
-    const $listOfItems = document.querySelectorAll('li');
-    const $listOfError = document.querySelectorAll('small');
-
-    if(!(isUnableToEnter()))
-    {
-        $button.disabled = false;
-        $button.classList.remove("disable");
-    }
-});
-
-
-
-
-
-
-
-
